62e48c273f48c58272d4c253bd4194f9dcb6d325
KBCHANUKAJ <chanukakbc@gmail.com>
2023-09-11 22:48:46 +0530

    Create about.php

---
+++ about.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About This Assignment</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container mt-5">
+        <h1>About This Assignment</h1>
+
+        <h2>Answers to Questions:</h2>
+        <ul>
+            <li>PHP Version Installed on Mercury: <?php echo phpversion(); ?></li>
+            <li>Tasks I Have Not Attempted or Not Completed:
+                <ul>
+                    <li>I have not implemented user authentication and security features for the system.</li>
+                </ul>
+            </li>
+            <li>Special Features Implemented:
+                <ul>
+                    <li>I have created a job vacancy posting and searching system using PHP and MySQL.</li>
+                    <li>Implemented validation for job posting and searching forms.</li>
+                    <li>Stored job vacancy data in a MySQL database.</li>
+                    <li>Created a search feature to find job vacancies based on job titles.</li>
+                    <li>Generated dynamic HTML content based on user input and database data.</li>
+                </ul>
+            </li>
+        </ul>
+
+        <!-- Add a link to return to the Home page -->
+        <p><a class="btn btn-primary" href="index.php">Return to Home Page</a></p>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
